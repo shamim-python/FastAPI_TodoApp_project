215174730fac093051e0ad9e84900de9c07e5764
Shamim Hossain <ummah45624@gmail.com>
2026-09-21 09:47:32 +0600

    first commit

--- database.py
+++ database.py
@@ -5,7 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/TodoApplicationDatabase'
 SQLALCHEMY_DATABASE_URL='sqlite:///./todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
