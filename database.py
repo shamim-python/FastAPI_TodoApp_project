d3491c4bac42820c82f8b8928822ef8b8279a0ac
Shamim Hossain <ummah45624@gmail.com>
2026-09-22 11:55:57 +0600

    fix database engine

--- database.py
+++ database.py
@@ -6,8 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL='postgresql://postgres.mwxtjftlwvimicwexdwu:shamimabcd12@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
